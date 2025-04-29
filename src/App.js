dae9bfddc2eba17bd4217b6a460a74c0ab1073e9
heiro <dev.heiro@gmail.com>
2025-04-29 17:55:58 +0900

    feat : 관리자-전체사원UI /사원-로그인UI 등 구축

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 
 import AdminAddEmployeePage from "./pages/admin/AdminAddEmployee";
 import AdminIndexPage from "./pages/admin/AdminIndex";
+import AdminManageEmployeePage from "./pages/admin/AdminManageEmployee";
+import UserIndexPage from "./pages/user/UserIndex";
+import UserWorkspacePage from "./pages/user/UserWorkspace";
 
 function App() {
   const router = createBrowserRouter([
+    {
+      path: "/user/index",
+      element: <UserIndexPage />,
+    },
+    {
+      path: "/user/workspace",
+      element: <UserWorkspacePage />,
+    },
     {
       path: "/admin/index",
       element: <AdminIndexPage />,
@@ -13,6 +24,10 @@ function App() {
     {
       path: "/admin/employees/add",
       element: <AdminAddEmployeePage />,
+    },
+    {
+      path: "/admin/employees/manage",
+      element: <AdminManageEmployeePage />,
     },
   ]);
 
